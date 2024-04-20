3c9821ec3cd3027ab44239338bd6c6a70d667a3e
anicmarko <anic.it36.2022@gmail.com>
2024-04-21 00:29:34 +0200

    feat: Authentification

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
+
+const DashboardPage = () => {
+  return (
+    <div>
+      <p>Dashboard</p>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
+
+export default DashboardPage;
