15ec7dbc3242ee11ce96d5d536cdaa51ebc2caa9
anicmarko <anic.it36.2022@gmail.com>
2024-06-14 00:23:54 +0200

    feat: Checking if logged in user is subscribed

---
+++ lib/subscription.ts
@@ -0,0 +1,29 @@
+import { auth } from "@clerk/nextjs/server";
+
+import prismadb from "@/lib/prismadb";
+
+const DAY_TO_MS= 86_400_000;
+
+export const checkSubscription = async () => {
+    const {userId} = auth();
+
+    if(!userId) return false;
+
+    const userSubscription= await prismadb.userSubscription.findUnique({
+        where: {
+            userId
+        },
+        select:{
+            stripePriceId: true,
+            stripeSubscriptionId: true,
+            stripeCustomerId: true,
+            stripeCurrentPeriodEnd: true,
+        }
+    });
+
+    if(!userSubscription) return false;
+
+    const isValid = userSubscription.stripePriceId && userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAY_TO_MS > Date.now();
+
+    return !!isValid;
+};
