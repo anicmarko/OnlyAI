c1df639f3a7a4a7467cff91efe0bbe3f4cb8894b
anicmarko <anic.it36.2022@gmail.com>
2024-06-14 18:21:37 +0200

    fix: Protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,8 @@
 import { createRouteMatcher, clerkMiddleware } from "@clerk/nextjs/server";
 
+
 const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)','/api/webhook  '
+  '/dashboard','/api/webhook ','/image', '/code','/video','/music','/conversation',
 ]);
 
 export default clerkMiddleware((auth, req) => {
